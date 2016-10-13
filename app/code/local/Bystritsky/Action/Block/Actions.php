944d8b9f51500994bc866b62a62ad0309b2a264f
User <user@example.com>
2016-10-13 12:01:44 +0300

    Added correct time filtring

--- app/code/local/Bystritsky/Action/Block/Actions.php
+++ app/code/local/Bystritsky/Action/Block/Actions.php
@@ -6,8 +6,14 @@ class Bystritsky_Action_Block_Actions extends Mage_Core_Block_Template
     public function getActionsCollection()
     {
         /** @var Bystritsky_Action_Model_Resource_Action_Collection $actions */
+
         $actions = Mage::getModel('bystritsky_action/action')->getCollection();
-        $actions->addFilter('is_active', '1')->setOrder('start_datetime', 'DESC');
+        $actions
+            ->addFieldToFilter('is_active', '1')
+            ->addFieldToFilter('start_datetime', ['lt' => Mage::getModel('core/date')->gmtDate()])
+            ->addFieldToFilter('end_datetime', [['gt' => Mage::getModel('core/date')->gmtDate()], 'Null'])
+            ->setOrder('start_datetime', 'DESC');
+        Mage::log((string) $actions->getSelect());
         return $actions;
     }
 }
