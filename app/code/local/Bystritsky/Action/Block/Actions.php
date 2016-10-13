26f71d314fe6b5fa7e6cab4ed07561e3c3add7fd
User <user@example.com>
2016-10-13 16:02:21 +0300

    Added pagination

--- app/code/local/Bystritsky/Action/Block/Actions.php
+++ app/code/local/Bystritsky/Action/Block/Actions.php
@@ -1,19 +1,38 @@
 <?php
 
-class Bystritsky_Action_Block_Actions extends Mage_Core_Block_Template
+class Bystritsky_Action_Block_Actions extends Mage_Catalog_Block_Product_Abstract
 {
 
     public function getActionsCollection()
     {
         /** @var Bystritsky_Action_Model_Resource_Action_Collection $actions */
 
+        $page = $this->getRequest()->getParam('p', 1);
+        $limit = $this->getRequest()->getParam('limit', 10);
         $actions = Mage::getModel('bystritsky_action/action')->getCollection();
         $actions
             ->addFieldToFilter('is_active', '1')
             ->addFieldToFilter('start_datetime', ['lt' => Mage::getModel('core/date')->gmtDate()])
             ->addFieldToFilter('end_datetime', [['gt' => Mage::getModel('core/date')->gmtDate()], ['null' => 'true']])
+            ->setPageSize($limit)
+            ->setCurPage($page)
             ->setOrder('start_datetime', 'DESC');
         Mage::log((string) $actions->getSelect());
         return $actions;
+    }
+
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        $pager = $this->getLayout()->createBlock('page/html_pager', 'custom.pager');
+        $pager->setAvailableLimit([10 => 10, 20 => 20, 'all'=>'all']);
+        $pager->setCollection($this->getActionsCollection());
+        $this->setChild('pager', $pager);
+        return $this;
+    }
+
+    public function getPagerHtml()
+    {
+        return $this->getChildHtml('pager');
     }
 }
