685c2f3d4eda1e6ea4fb4862af0309e4d1ed8ec3
User <user@example.com>
2016-10-17 09:33:30 +0300

    Created base blank admin page

---
+++ app/code/local/Bystritsky/Action/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Bystritsky_Action_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/Bystritsky/Action/controllers/Adminhtml/ActionsController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Bystritsky_Action_Adminhtml_ActionsController extends Mage_Adminhtml_Controller_Action
+{
+
+    public function indexAction()
+    {
+        echo '<h1>News Module: Admin section</h1>';
+    }
+
+}
