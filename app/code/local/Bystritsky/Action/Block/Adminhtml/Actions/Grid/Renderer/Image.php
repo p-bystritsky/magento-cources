7547b35da6012aebe536e673eda979f5ed833b1c
User <user@example.com>
2016-10-18 13:25:57 +0300

    Fixed null-image preview in admin grid

--- app/code/local/Bystritsky/Action/Block/Adminhtml/Actions/Grid/Renderer/Image.php
+++ app/code/local/Bystritsky/Action/Block/Adminhtml/Actions/Grid/Renderer/Image.php
@@ -8,10 +8,14 @@ class Bystritsky_Action_Block_Adminhtml_Actions_Grid_Renderer_Image extends Mage
     protected function _getValue(Varien_Object $row)
     {
         $val = $row->getData($this->getColumn()->getIndex());
-        $val = str_replace("no_selection", "", $val);
-        $helper = Mage::helper('bystritsky_action');
-        $url = $helper->getImageUrl($val);
-        $out = "<img src=". $url ." width='60px'/>";
-        return $out;
+        if ($val) {
+            $val = str_replace("no_selection", "", $val);
+            $helper = Mage::helper('bystritsky_action');
+            $url = $helper->getImageUrl($val);
+            $out = "<img src=". $url ." width='60px'/>";
+            return $out;
+        } else {
+            return null;
+        }
     }
 }
