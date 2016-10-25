b020bb604f49ccde49eb90308cce883b0c81293c
User <user@example.com>
2016-10-25 11:28:55 +0300

    Added 404 error for viewing completed / not started / not active actions

--- app/code/local/Bystritsky/Action/Block/View.php
+++ app/code/local/Bystritsky/Action/Block/View.php
@@ -7,6 +7,10 @@ class Bystritsky_Action_Block_View extends Mage_Core_Block_Template
         $id = $this->getRequest()->getParam('id');
         $actions = Mage::getModel('bystritsky_action/action');
         $action = $actions->load($id);
-        return $action;
+        if ($action->getIsActive() && $action->getStatus() == Bystritsky_Action_Model_Action::ACTING) {
+            return $action;
+        } else {
+            Mage::app()->getResponse()->setRedirect('/no-route.html');
+        }
     }
 }
